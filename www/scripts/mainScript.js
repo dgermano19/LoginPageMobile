865c903e76173b7dea737f8a7fd81cdb324b4cda
dgermano19 <dgermanoprofissional@gmail.com>
2023-10-21 07:55:42 +0900

    login data collected

--- www/scripts/mainScript.js
+++ www/scripts/mainScript.js
@@ -1,2 +1,8 @@
-var titulo = document.getElementById("mainTitle");
-titulo.textContent = "Mobile App";
+const email = document.getElementById("emailField");
+const password = document.getElementById("passwordField");
+const submitBtn = document.getElementById("submitButton");
+
+submitBtn.addEventListener("click", function(){
+    event.preventDefault();
+    document.getElementById("demo").innerHTML = email.value+", "+password.value;
+});
