57a135fb56fdc74a9621a81f88757816ee9d1289
dgermano19 <dgermanoprofissional@gmail.com>
2023-10-21 09:02:02 +0900

    Login: working\; Homepage: created logoutButton: working

--- www/scripts/mainScript.js
+++ www/scripts/mainScript.js
@@ -2,7 +2,15 @@ const email = document.getElementById("emailField");
 const password = document.getElementById("passwordField");
 const submitBtn = document.getElementById("submitButton");
 
-submitBtn.addEventListener("click", function(){
+submitBtn.addEventListener("click", function(event){
+    //Impede que a página reinicie automaticamente ao tocar no botão
     event.preventDefault();
-    document.getElementById("demo").innerHTML = email.value+", "+password.value;
+
+    if( email.value !="" && password.value !=""){
+        if( email.value=="mymail@mail.com" && password.value=="admin"){
+            window.location.href = "homePage.html";
+        }else{ alert("wrong data");}
+    }else{ alert("empty fields");}
+
+
 });
